11d9fcb0a74896d69875176d808618602f08a4a4
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-30 11:22:47 -0400

    define EmployeeDetailsComponent

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,4 +1,7 @@
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../employee';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-details',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  employee: Employee;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private employeeService: EmployeeService
+  ) {}
 
   ngOnInit(): void {
+    this.employee = new Employee();
+    this.id = this.route.snapshot.params.id;
+    this.employeeService.getEmployee(this.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.employee = data;
+      },
+      (error) => console.log(error)
+    );
   }
 
+  list = () => {
+    this.router.navigate(['employees']);
+  };
 }
