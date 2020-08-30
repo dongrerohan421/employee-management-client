3922a591b0712732cdd692f00df1f1e03f86f569
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-30 11:40:50 -0400

    create and handle a new employee form data

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../employee';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  employee: Employee = new Employee();
+  submitted = false;
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  newEmployee(): void {
+    this.submitted = false;
+    this.employee = new Employee();
   }
 
+  save = () => {
+    this.employeeService.createEmployee(this.employee).subscribe(
+      (data) => {
+        console.log(data);
+        this.employee = new Employee();
+        this.gotoList();
+      },
+      (error) => console.log(error)
+    );
+  };
+
+  onSubmit = () => {
+    this.submitted = true;
+    this.save();
+  };
+
+  gotoList = () => {
+    this.router.navigate(['/employees']);
+  };
 }
