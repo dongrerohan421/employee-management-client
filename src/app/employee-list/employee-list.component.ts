c76ba0d451cec271ee96145ab43d9d17144d975e
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-30 10:57:31 -0400

    define EmployeeListComponent

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,9 @@
+import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
+import { EmployeeService } from './../employee.service';
+import { Employee } from './../employee';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor() { }
+  employees: Observable<Employee[]>;
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.reloadData();
   }
 
+  reloadData = () => {
+    this.employees = this.employeeService.getEmployeeList();
+  };
+
+  deleteEmployeeId = (id: number) => {
+    this.employeeService.deleteEmployee(id).subscribe(
+      (data) => {
+        console.log(data);
+        this.reloadData();
+      },
+      (error) => console.log(error)
+    );
+  };
+
+  employeeDetails = (id: number) => {
+    this.router.navigate(['details', id]);
+  };
 }
