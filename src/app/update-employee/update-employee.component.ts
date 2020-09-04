038cd3c071b6ebfdf27a070ce798fa61e42aa2cc
dongrerohan421 <dongrerohan421@gmail.com>
2020-09-04 10:23:36 -0400

    implement UpdateEmployeeComponent

--- src/app/update-employee/update-employee.component.ts
+++ src/app/update-employee/update-employee.component.ts
@@ -1,4 +1,8 @@
+import { EmployeeService } from './../employee.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from './../employee';
 import { Component, OnInit } from '@angular/core';
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-update-employee',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-employee.component.css']
 })
 export class UpdateEmployeeComponent implements OnInit {
+  id: number;
+  employee: Employee;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private employeeService: EmployeeService
+  ) {}
 
   ngOnInit(): void {
+    this.employee = new Employee();
+    this.id = this.route.snapshot.params.id;
+    this.employeeService.getEmployee(this.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.employee = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  updateEmployee = () => {
+    this.employeeService.updateEmployee(this.id, this.employee).subscribe(
+      (data) => {
+        console.log(data);
+        this.employee = new Employee();
+        this.gotoList();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+
+  onSubmit = () => {
+    this.updateEmployee();
+  };
+
+  gotoList = () => {
+    this.router.navigate(['/employees']);
+  };
 }
