e1f452f2b3eea5c0227dc3daf30a969c02527476
dongrerohan421 <dongrerohan421@gmail.com>
2020-09-04 11:08:33 -0400

    configure app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add', component: CreateEmployeeComponent },
+  { path: 'update/:id', component: UpdateEmployeeComponent },
+  { path: 'details/:id', component: EmployeeDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
